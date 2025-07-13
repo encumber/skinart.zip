f5be66e58a015499efaef206eefdfedda15c45f5
encumber <32603882+encumber@users.noreply.github.com>
2025-07-13 03:29:39 -0400

    Create artwork.js

---
+++ artwork.js
@@ -0,0 +1,52 @@
+
+    // Each artwork: [title, artist, desc, image]
+    const artworks = [
+        [
+          'Ocupado',
+          '182exe',
+          'sorry its ocupado <a href="https://namemc.com/ocupado">NameMC</a>',
+          'https://skinart.zip/img/ocupado.png'
+        ],
+        [
+          'A Silent Voice',
+          '182exe',
+          'Cherry Blossom falling down, inspired by the movie A Silent Voice. 10/10 would recommend watching it. <a href="https://namemc.com/Koe_No_Katachi">NameMC</a>',
+          'https://skinart.zip/img/koe_no_katachi.png'
+        ],
+        [
+          'Wanacry',
+          '182exe',
+          'One of the most iconic malware icons. <a href="https://namemc.com/MalwareTech">NameMC</a>',
+          'https://skinart.zip/img/malwaretech.png'
+        ],
+        [
+          'Scooby Doo',
+          'Zubook',
+          'Scooby and the gang Weezer style <a href="https://namemc.com/NATHANLEL">NameMC</a>',
+          'https://skinart.zip/img/nathanlel.png'
+        ],
+        [
+          'Pokemada',
+          'NATHANLEL',
+          'Gotta catch em all. <a href="https://namemc.com/Pokemada">NameMC</a>',
+          'https://skinart.zip/img/pokemada.png'
+        ],
+        [
+          'Zillow',
+          'NATHANLEL',
+          'Best place to buy a house. <a href="https://namemc.com/Zillow">NameMC</a>',
+          'https://skinart.zip/img/zillow.png'
+        ],
+        [
+          'Space',
+          'Alien',
+          'A view from the moon. <a href="https://namemc.com/Aliennn">NameMC</a>',
+          'https://skinart.zip/img/aliennn.png'
+        ],
+        [
+          'Wide Steve',
+          'ProblematicPatagon',
+          'Well this is awkward, i seem to be wide. <a href="https://namemc.com/ProblematicPatagon">NameMC</a>',
+          'https://skinart.zip/img/problematicparagon.png'
+        ]
+      ];
