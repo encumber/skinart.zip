4997f5d26b4cd7b64bcb22cdfabf00add8d53589
encumber <32603882+encumber@users.noreply.github.com>
2025-07-13 03:39:03 -0400

    updated js

--- artwork.js
+++ artwork.js
@@ -5,48 +5,54 @@
           'Ocupado',
           '182exe',
           'sorry its ocupado <a href="https://namemc.com/ocupado">NameMC</a>',
-          'https://skinart.zip/img/ocupado.png'
+          'https://raw.githubusercontent.com/encumber/skinart.zip/refs/heads/main/img/ocupado.png'
         ],
         [
           'A Silent Voice',
           '182exe',
           'Cherry Blossom falling down, inspired by the movie A Silent Voice. 10/10 would recommend watching it. <a href="https://namemc.com/Koe_No_Katachi">NameMC</a>',
-          'https://skinart.zip/img/koe_no_katachi.png'
+          'https://raw.githubusercontent.com/encumber/skinart.zip/refs/heads/main/img/koe_no_katachi.png'
         ],
         [
           'Wanacry',
           '182exe',
           'One of the most iconic malware icons. <a href="https://namemc.com/MalwareTech">NameMC</a>',
-          'https://skinart.zip/img/malwaretech.png'
+          'https://raw.githubusercontent.com/encumber/skinart.zip/refs/heads/main/img/malwaretech.png'
         ],
         [
           'Scooby Doo',
           'Zubook',
           'Scooby and the gang Weezer style <a href="https://namemc.com/NATHANLEL">NameMC</a>',
-          'https://skinart.zip/img/nathanlel.png'
+          'https://raw.githubusercontent.com/encumber/skinart.zip/refs/heads/main/img/nathanlel.png'
         ],
         [
           'Pokemada',
           'NATHANLEL',
           'Gotta catch em all. <a href="https://namemc.com/Pokemada">NameMC</a>',
-          'https://skinart.zip/img/pokemada.png'
+          'https://raw.githubusercontent.com/encumber/skinart.zip/refs/heads/main/img/pokemada.png'
         ],
         [
           'Zillow',
           'NATHANLEL',
           'Best place to buy a house. <a href="https://namemc.com/Zillow">NameMC</a>',
-          'https://skinart.zip/img/zillow.png'
+          'https://raw.githubusercontent.com/encumber/skinart.zip/refs/heads/main/img/zillow.png'
         ],
         [
           'Space',
           'Alien',
           'A view from the moon. <a href="https://namemc.com/Aliennn">NameMC</a>',
-          'https://skinart.zip/img/aliennn.png'
+          'https://raw.githubusercontent.com/encumber/skinart.zip/refs/heads/main/img/aliennn.png'
         ],
         [
           'Wide Steve',
           'ProblematicPatagon',
           'Well this is awkward, i seem to be wide. <a href="https://namemc.com/ProblematicPatagon">NameMC</a>',
-          'https://skinart.zip/img/problematicparagon.png'
+          'https://raw.githubusercontent.com/encumber/skinart.zip/refs/heads/main/img/problematicparagon.png'
+        ],
+        [
+          'LilyPichu',
+          'NATHANLEL',
+          'LilyPichu <a href="https://namemc.com/LilyPichu">NameMC</a>',
+          'https://raw.githubusercontent.com/encumber/skinart.zip/refs/heads/main/img/lilypichu.png'
         ]
       ];
